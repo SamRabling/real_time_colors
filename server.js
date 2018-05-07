695d2c05017aec28b739983578a9455b0a5147ea
Samantha Rabling <sjarillorabling@gmail.com>
2018-05-07 15:55:17 -0700

    finished html

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const server = app.listen(4000);
+const io = require("socket.io")(server);
+
+app.use(express.static(__dirname + "/public"));
+
+var default_color = "green";
+
+io.on("connection", function (socket) {
+
+    socket.emit('colour', {color: color}); 
+    socket.on('button_clicked', function (data) {
+        color = data.color;
+        io.emit('colour', {color:color});
+    });
+});
